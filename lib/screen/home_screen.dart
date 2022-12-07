64f676612c292c9a5266bc509eaaded2cd72c5dd
Shamimx2016 <shamimbd621@gmail.com>
2022-12-07 23:20:09 +0600

    new app

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  List catNames = [
+    "Category",
+    "Classes",
+    "Free Course",
+    "BooksStore",
+    "Live Course",
+    "Leaderboard",
+  ];
+  List<Color> catColors = [
+    Color(0xFFFFCF2F),
+    Color(0xFF6FE08D),
+    Color(0xFF61BDFD),
+    Color(0xFFFC7C7f),
+    Color(0xFFCB84FB),
+    Color(0xFF78E667),
+  ];
+
+  List<Icon> catIcons = [
+    Icon(
+      Icons.category,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.video_library,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.assessment,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.store,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.play_circle_fill,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.emoji_events,
+      color: Colors.white,
+      size: 30,
+    ),
+  ];
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            decoration: BoxDecoration(
+              color: Color(0xFF674AEF),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.dashboard,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.only(left: 3, bottom: 15),
+                  child: Text(
+                    "Hi,Programmer",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1,
+                      wordSpacing: 2,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 5,
+                    bottom: 20,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search hare",
+                      hintStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size: 25,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 20,
+              left: 15,
+              right: 15,
+            ),
+            child: Column(
+              children: [
+                GridView.builder(
+                  itemCount: catNames.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.1,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: catColors[index],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: catIcons[index],
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          catNames[index],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black.withOpacity(0.7),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
